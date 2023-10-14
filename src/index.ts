74b4273b79532955d9a1a072e5e800d46f7a44d8
Matorry <rodri.martinceron@gmail.com>
2023-10-14 10:14:21 +0200

    Add db connect, index and app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-
+import 'dotenv/config';
+import http from 'http';
+import { app } from './app.js';
+import { dbConnect } from './db/db.connect.js';
+const PORT = process.env.PORT ?? 8000;
+const server = http.createServer(app);
+dbConnect()
+  .then((mongoose) => {
+    server.listen(PORT);
+    console.log('Connected to Data Base', mongoose.connection.db.databaseName);
+  })
+  .catch((error) => {
+    server.emit('error', error);
+  });
+server.on('listening', () => {
+  console.log('Listening on port ' + PORT);
+});
+server.on('error', (error) => {
+  console.log(`Error ${error.message}`);
+});
